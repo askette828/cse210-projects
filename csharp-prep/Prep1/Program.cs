8ea94c1749e3396006000f2fbaef464138256e4f
Dias Kaisaruly <diaskaisaruly@Diass-MacBook-Air.local>
2025-09-21 16:47:41 -0600

    submitted prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your surname?");
+        string surname = Console.ReadLine();
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }

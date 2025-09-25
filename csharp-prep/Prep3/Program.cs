85a2a6c3e1a2f0e9924461d7766d3bce0db5ad05
Dias Kaisaruly <diaskaisaruly@Diass-MacBook-Air.local>
2025-09-25 09:27:37 -0600

    submitted prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int i, magicNum, num = 0;
+        string ans; 
+        do
+        {
+            i = 1;
+            Console.Write("What is your magic number? ");
+            magicNum = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your guess? ");
+            num = int.Parse(Console.ReadLine());
+
+            while (magicNum != num)
+            {
+                if (magicNum > num)
+                {
+                    Console.WriteLine("Higer");
+                    Console.Write("What is your guess? ");
+                    num = int.Parse(Console.ReadLine());
+                    i++;
+
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("What is your guess? ");
+                    num = int.Parse(Console.ReadLine());
+                    i++;
+                }
+            }
+            Console.WriteLine($"You guessed it! You have tried {i} times. Would you like to play again? ");
+            ans = Console.ReadLine();
+        }
+        while (ans == "yes");
     }
 }

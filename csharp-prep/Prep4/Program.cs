68502b3e74874fce6a1d53698e723e64d44842ca
Dias Kaisaruly <diaskaisaruly@Diass-MacBook-Air.local>
2025-09-25 23:10:29 -0600

    submitting prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+        int positiveMin = numbers.Where(n => n > 0).Min(); 
+        Console.WriteLine("The sum is " + numbers.Sum());
+        Console.WriteLine("The average is " + numbers.Average());
+        Console.WriteLine("The largest number is " + numbers.Max());
+        Console.WriteLine($"The smallest positive number is {positiveMin}");
+        Console.WriteLine(" The sorted list is:");
+
+        numbers.Sort();
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
     }
 }
